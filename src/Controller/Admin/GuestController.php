7fd2aa473182dc1ea9d58a1bb89389ff29fc1719
Quentin D. <quentin.didelot13@gmail.com>
2024-11-26 08:19:34 +0100

    Step 4 - New functionnalities
    - Add a new guest : done

--- src/Controller/Admin/GuestController.php
+++ src/Controller/Admin/GuestController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Form\GuestType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -29,4 +31,26 @@ class GuestController extends AbstractController
             'guests' => $guests,
         ]);
     }
+
+    #[Route('/admin/guest/add', name: 'admin_guest_add')]
+    public function add(Request $request): Response {
+        $guest = new User();
+        $form = $this->createForm(GuestType::class, $guest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->userPasswordHasher->hashPassword($guest, $guest->getPassword());
+            $guest->setPassword($password);
+            $guest->setRoles(['ROLE_USER']);
+            $guest->setAdmin(false);
+            $guest->setRestricted(false);
+
+            $this->entityManager->persist($guest);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_guest_index');
+        }
+    return $this->render('admin/guest/add.html.twig', ['form' => $form->createView()]);
+    }
+
 }
