9afc2b6da73b7276996614a215da76e3dec9defe
Quentin D. <quentin.didelot13@gmail.com>
2024-11-27 18:57:02 +0100

    Step 4 - Login guests with restriction

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
     #[Route(path: '/login', name: 'admin_login')]
     public function login(AuthenticationUtils $authenticationUtils)
     {
